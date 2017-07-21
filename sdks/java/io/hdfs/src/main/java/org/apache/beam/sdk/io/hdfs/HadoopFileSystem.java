cosmoskitten/beam

[BEAM-59] Add FileSystems#matchNewResource
The new FileSystems API needs a way to generate a ResourceId for a
resource that does not exist. This does not come up in sources, because
we typically are just matching existing files. However, sinks need the
ability to reference a new directory (say, in order to create it).

Couldn't think of anything better than a simple function that says
"treat this string as a full resource path with the specified type",
which is what FileSystems#matchNewResource is.

sdks/java/io/hdfs/src/main/java/org/apache/beam/sdk/io/hdfs/HadoopFileSystem.java
@@ -25,6 +25,7 @@ import java.util.List;
 import org.apache.beam.sdk.io.FileSystem;
 import org.apache.beam.sdk.io.fs.CreateOptions;
 import org.apache.beam.sdk.io.fs.MatchResult;
+import org.apache.beam.sdk.io.fs.ResourceId;
 
 /**
  * Adapts {@link org.apache.hadoop.fs.FileSystem} connectors to be used as
@@ -66,6 +67,11 @@ class HadoopFileSystem extends FileSystem<HadoopResourceId> {
 
   @Override
   protected void delete(Collection<HadoopResourceId> resourceIds) throws IOException {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  protected ResourceId matchNewResource(String singleResourceSpec, boolean isDirectory) {
     throw new UnsupportedOperationException();
   }
 }

peihe/incubator-beam

sdks/java/io/hdfs/src/main/java/org/apache/beam/sdk/io/hdfs/HadoopFileSystem.java
@@ -68,4 +68,9 @@ class HadoopFileSystem extends FileSystem<HadoopResourceId> {
   protected void delete(Collection<HadoopResourceId> resourceIds) throws IOException {
     throw new UnsupportedOperationException();
   }
+
+  @Override
+  protected HadoopResourceId matchNewResource(String singleResourceSpec, boolean isDirectory) {
+    throw new UnsupportedOperationException();
+  }
 }